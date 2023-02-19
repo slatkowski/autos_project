2217b75ac3b2678cbdef9361bf5fb13b77dd9c76
Jakub Walczak <jakub_walczak@yahoo.com>
2023-02-19 15:34:43 +0100

    Version 5.0 - Balanced RFC added to Python file, updated requirements

--- Premium_cars_classification.py
+++ Premium_cars_classification.py
@@ -9,12 +9,13 @@ import math
 import warnings
 import random
 import xgboost as xgb
-import joblib
-warnings.filterwarnings("ignore")
 from collections import Counter
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, ConfusionMatrixDisplay
+from imblearn.ensemble import BalancedRandomForestClassifier
+
+warnings.filterwarnings("ignore")
 
 
 ''' Dataset creation, verification of table's contents.
@@ -58,7 +59,7 @@ print(df.info())
 
 ''' First descriptive statistics of DataFrame (values round to two places after a comma to better readability),
 outliers searching.
-Unfortunately, there are many outliers - f. e. maximum value of column "price" overpasses 2 bilions of euro, 
+Unfortunately, there are many outliers - f. e. maximum value of column "price" overpasses 2 billions of euro, 
 we have value (or even values) with price = 0, in column "yearOfRegistration" we have cars "registered" in year 
 1000 and 9999.
 Also column "kilometer" may show not enough variance - max value (150000) appears as a median.'''
@@ -84,7 +85,6 @@ frame_counter(str_cols)
 '''Displaying columns with NaN-s.
 Columns "vehicleType", "gearbox", "model", "fuelType" and "notRepairedDamage" contain missing values.'''
 
-plt.figure(figsize=(10, 6))
 sns.displot(
     data=df.isna().melt(value_name="missing"),
     y="variable",
@@ -136,7 +136,7 @@ with price in price bracket 200-60000 euros and engine power (in HP/PS) bracket 
 (some of the brands have more powerful models, but they are not common and can be considered as outliers).'''
 
 df = df[(df['yearOfRegistration'] >= 2001) & (df['yearOfRegistration'] <= 2016)]
-df = df[(df['price'] >= 100) & (df['price'] <= 60000)]
+df = df[(df['price'] >= 200) & (df['price'] <= 60000)]
 df = df[(df['powerPS'] >= 39) & (df['powerPS'] <= 550)]
 
 '''Replacing hidden missing values as NaN-s (in German: "andere" - "other").'''
@@ -163,14 +163,14 @@ str_cols = df.select_dtypes(include='object')
 frame_counter(str_cols)
 
 '''Removing unimportant columns.
-We should remove the columns which don't bring useful informations (referring to dates, 'name'), 
+We should remove the columns which don't bring useful information (referring to dates, 'name'), 
 not referring to cars' properties ('abtest' - it brings us information about the version of the website), 
 undifferentiated ('nrOfPictures', 'seller' and 'offerType') and being a data leakage ('model').
 '''
 
 df.drop(columns=['dateCrawled', 'name', 'monthOfRegistration', 'seller',
                  'model', 'dateCreated', 'lastSeen', 'nrOfPictures',
-                 'offerType', 'abtest', 'notRepairedDamage'], inplace=True)
+                 'offerType', 'abtest'], inplace=True)
 
 print(df.columns)
 
@@ -181,7 +181,6 @@ print(df.info())
 description = df.describe().apply(lambda c: c.apply('{0:.2f}'.format))
 print(description)
 
-plt.figure(figsize=(10, 6))
 sns.displot(
     data=df.isna().melt(value_name="missing"),
     y="variable",
@@ -219,8 +218,8 @@ def nan_filler(frame):
 
 nan_filler(df)
 
-'''Columns' 'postalCode' and 'kilometer' values don't bring us useful informations, when they're continuous.
-Good idea might be transformating them into discrete.'''
+'''Columns' 'postalCode' and 'kilometer' values don't bring us useful information, when they're continuous.
+Good idea might be transforming them into discrete.'''
 
 bins = [0, 9999, 19999, 29999, 39999,
         49999, 59999, 69999, 79999, 89999, 99999]
@@ -247,10 +246,10 @@ frame_counter(str_cols)
 
 '''On normalized plots we can see that:
 1. Non-premium cars have huge advantage in hatchback (kleinwagen) and bus models.
-Premium models are relatively more likely to be limousines, cabrios, coupes and combis.
+Premium models are relatively more likely to be limousines, cabriolets, coupes and estates (kombi).
 SUVs frequency are almost the same with a small advantage of premium brands.
 2. Advantage of manual gearboxes is merely visible in premium brands group and much bigger in non-premium category.
-3. Distrubutions of first registration dates are quite similar. The most models from each group come from
+3. Distributions of first registration dates are quite similar. The most models from each group come from
 years 2001-2009. Bigger amount of relatively new models (from years 2014-2016) are non-premium.
 4. In column 'powerPS' we can see that there's not much samples of cars with power over 550 HP.
 However, we can see that distributions have similar shape, but different values - 
@@ -263,7 +262,8 @@ However, there's no region in which we have big advantage of any of the groups.
 9. Price distribution of premium brands is much more flattened.
 Non-premium brands' distribution reach their peak earlier and falls rapidly. 
 From about 10000 euros premium cars are two times more numerous.
-That situation is quite similar in column 'priceOf1PS', but differences between both distibutions are closer.'''
+That situation is quite similar in column 'priceOf1PS', but differences between both distibutions are closer.
+10. Column 'notRepairedDamage', because of lack of variance, will be useless in modelling and should be dropped.'''
 
 _ = df.drop(columns=['brand'])
 cols_wo_brands = list(_.columns)
@@ -272,23 +272,22 @@ rotation = 45
 legend = (1, 1)
 figsize = (6, 3.5)
 
-for i, col in enumerate(df[cols_wo_brands]):
-    plt.figure(i)
+for i in df[cols_wo_brands]:
     fig, ax = plt.subplots(figsize=figsize)
-    if df[col].dtype == 'object':
-        ax = sns.histplot(x=col, data=df,
+    if df[i].dtype == 'object':
+        ax = sns.histplot(x=i, data=df,
                           hue='brand', multiple="dodge",
                           stat='density', shrink=0.8, common_norm=False)
         sns.move_legend(ax, "upper left", bbox_to_anchor=legend)
         plt.xticks(rotation=rotation)
-    elif df[col].dtype == 'category':
-        ax = sns.histplot(x=col, data=df,
+    elif df[i].dtype == 'category':
+        ax = sns.histplot(x=i, data=df,
                           hue='brand', multiple="dodge",
                           stat='density', shrink=0.8, common_norm=False)
         sns.move_legend(ax, "upper left", bbox_to_anchor=legend)
         plt.xticks(rotation=rotation)
     else:
-        ax = sns.kdeplot(x=col, data=df,
+        ax = sns.kdeplot(x=i, data=df,
                          hue='brand', common_norm=False)
         sns.move_legend(ax, "upper left", bbox_to_anchor=legend)
         plt.xticks(rotation=rotation)
@@ -296,6 +295,8 @@ for i, col in enumerate(df[cols_wo_brands]):
     plt.tight_layout()
     plt.show()
 
+
+df.drop(columns='notRepairedDamage', inplace=True)
 
 '''Pivot table displays descriptive statistics divided into brands groups.
 Basing on mean, standard deviation and our previous observations we can see that the variables
@@ -384,14 +385,14 @@ imbalance = counter[0] / counter[1]
 print(f'Imbalance of classes equals: {imbalance}.')
 
 '''Modelling - chosen XGBClassifier instance (process of tuning and selection of algorithm - in notebook).
-The algorithm we've chosen to solve classification problem is XGBClassifier. It's efficient ensemble classifier,
-keeping what's the best from random forest idea and adding gradient boosting, which advantage 
-is improving results of learning after each boosting step.
+The 1st algorithm we've chosen to solve classification problem is XGBClassifier. It's efficient ensemble classifier,
+keeping what's the best from random forest idea and adding gradient boosting, which advantage is improving results 
+of learning after each boosting step.
 This problem isn't linear and therefore usage of more complicated algorithm could bring more profits.'''
 
 xgb_model = xgb.XGBClassifier(
     max_depth=8,  # depth of the tree estimator
-    n_estimators=400,  # number of boosting steps/epochs
+    n_estimators=300,  # number of boosting steps/epochs
     learning_rate=0.3,  # shrinkage rate of the feature weights after each boosting step
     objective='binary:logistic',  # objective - logistic regression for binary classification
     eval_metric='error',  # evaluation metrics
@@ -400,11 +401,12 @@ xgb_model = xgb.XGBClassifier(
     scale_pos_weight=imbalance  # balance control rate
 )
 
+print("XGBClassifier's training:")
 start = time.time()
 xgb_model.fit(
     X_train_sc, y_train,
     eval_set=[(X_train_sc, y_train), (X_val_sc, y_val)],
-    early_stopping_rounds=20,  # training interruption after 20 steps with no progress
+    early_stopping_rounds=15,  # training interruption after 15 steps with no progress
     verbose=10  # tells us about training progress every 10 steps
 )
 
@@ -412,20 +414,17 @@ xgb_model_train_pred = xgb_model.predict(X_train_sc)
 stop = time.time()
 mins = math.floor((stop - start) / 60)
 seconds = math.ceil((stop - start) % 60)
-print(f"Training time: {mins} mins and {seconds} s.")
+print(f"Training time: {mins}:{seconds} mins.")
 
 '''Scores in positive class - premium - are decent.
-About 91% of its cases have been detected (as it's indicated by recall in validation and test sets).
+About 90% of its cases have been detected (as it's indicated by recall in validation and test sets).
 The decrease of metrics is nominal in evaluation sets and the recall for true positives is still pretty good.'''
 
 
 xgb_model_val_pred = xgb_model.predict(X_val_sc)
-xgb_model_test_pred = xgb_model.predict(X_test_sc)
 print(f'Training metrics:\n\n{classification_report(y_train, xgb_model_train_pred)}')
 print('_____________________________________________________\n')
 print(f'Validation metrics:\n\n{classification_report(y_val, xgb_model_val_pred)}')
-print('_____________________________________________________\n')
-print(f'Test metrics:\n\n{classification_report(y_test, xgb_model_test_pred)}')
 
 
 '''Confusion matrices plotting.'''
@@ -434,29 +433,24 @@ brands = le.classes_
 
 
 def conf_matrix_show(estimator, prediction, truth, classes):
-    # 1. Drawing the plot, setting the plot's default size.
-    fig, ax = plt.subplots(figsize=(8, 8))
-    # 2. Creating the instance of confusion matrix from estimator and set, and displaying classes to predict.
+    # 1. Creating the instance of confusion matrix from estimator and set, and displaying classes to predict.
     ConfusionMatrixDisplay.from_estimator(estimator,
                                           prediction,
                                           truth,
                                           display_labels=classes,
-                                          ax=ax,
                                           values_format=' ')
-    # 3. Disabling grid - it makes confusion matrix much more readable.
+    # 2. Disabling grid - it makes confusion matrix much more readable.
     ax.grid(False)
     plt.show()
 
 
 conf_matrix_show(xgb_model, X_train_sc, y_train, brands)
 conf_matrix_show(xgb_model, X_val_sc, y_val, brands)
-conf_matrix_show(xgb_model, X_test_sc, y_test, brands)
 
-'''Model stopped having visible learning progress at about 160th estimator.'''
+'''Model stopped making visible learning progress at about 160th estimator.'''
 
 results = xgb_model.evals_result()
 
-plt.figure(figsize=(9, 8))
 plt.plot(results["validation_0"]["error"], label="Training error")
 plt.plot(results["validation_1"]["error"], label="Validation error")
 plt.axvline(xgb_model.best_iteration, color="gray", label="Optimal tree number")
@@ -473,7 +467,7 @@ xgb_importance = xgb_model.feature_importances_
 xgb_features = X.columns
 
 
-def plot_feature_importance(importance, names):
+def plot_feature_importance(importance, names, model_type):
     # 1. Create arrays from feature importance and feature names
     feature_importance = np.array(importance)
     feature_names = np.array(names)
@@ -488,17 +482,68 @@ def plot_feature_importance(importance, names):
     sns.barplot(x=fi_frame['feature_importance'],
                 y=fi_frame['feature_names'])
     # 6. Add chart labels
-    plt.title('Feature importance of model')
+    plt.title(f'Feature importance of {model_type}')
     plt.xlabel('Importance')
     plt.ylabel('Name')
     plt.show()
 
 
-plot_feature_importance(xgb_importance, xgb_features)
+plot_feature_importance(xgb_importance, xgb_features, xgb_model)
 
 
-'''Saving model to file.'''
+''' RandomForestClassifier is probably the most powerful Scikit-learn ensemble classifier. 
+BalancedRFC from Imbalanced-learn module is its variant created especially for imbalanced sets; 
+it randomly under-samples each boostrap sample to balance it.
+BRFC doesn't need normalized data.'''
 
-joblib.dump(xgb_model, 'xgb_model.pkl')
-model = joblib.load('xgb_model.pkl')
-print(model)
+
+brfc_model = BalancedRandomForestClassifier(
+    max_depth=18,  # depth of the tree in model
+    min_samples_split=4,  # min samples in node
+    min_samples_leaf=1,  # min samples in leaf
+    n_estimators=100,  # number of trees in forest
+    verbose=3
+)
+
+print("BalancedRFC's training:")
+start = time.time()
+brfc_model.fit(X_train, y_train)
+brfc_train_pred = brfc_model.predict(X_train)
+stop = time.time()
+mins = math.floor((stop - start) / 60)
+seconds = math.ceil((stop - start) % 60)
+print(f"Training time: {mins}:{seconds} mins.")
+
+'''Metrics are also solid. Model has even better recall, when it comes to positive class,
+but also has worse precision and that's why F1 score decreases'''
+
+brfc_val_pred = brfc_model.predict(X_val)
+print(f'Training metrics:\n\n{classification_report(y_train, brfc_train_pred)}')
+print('_____________________________________________________\n')
+print(f'Validation metrics:\n\n{classification_report(y_val, brfc_val_pred)}')
+
+
+'''This algorithm makes more false positive mistakes.'''
+
+conf_matrix_show(brfc_model, X_train, y_train, brands)
+conf_matrix_show(brfc_model, X_val, y_val, brands)
+
+'''BRFC features importance plotting.'''
+
+brfc_importance = brfc_model.feature_importances_
+brfc_features = X.columns
+plot_feature_importance(brfc_importance, brfc_features, brfc_model)
+
+'''As the summary we should display metrics of every model on test set.'''
+
+xgb_model_test_pred = xgb_model.predict(X_test_sc)
+print(f'XGB Classifier test metrics:\n\n{classification_report(y_test, xgb_model_test_pred)}')
+print('_____________________________________________________\n')
+brfc_test_pred = brfc_model.predict(X_test)
+print(f'Balanced RFC metrics:\n\n{classification_report(y_test, brfc_test_pred)}')
+
+
+'''At the end, let's plot confusion matrix of each model's test set prediction.'''
+
+conf_matrix_show(xgb_model, X_test_sc, y_test, brands)
+conf_matrix_show(brfc_model, X_test, y_test, brands)
